6bb0e15e59a1f396268d2774719718a674ff31d2
shitianshu <doglooksgood@gmail.com>
2021-02-17 23:32:40 +0800

    update

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -5,3 +5,17 @@ c.qt.highdpi = True
 c.content.proxy = 'http://127.0.0.1:8889'
 
 config.bind('<Space>', 'hint')
+
+config.bind('h', 'scroll left')
+config.bind('t', 'scroll down')
+config.bind('c', 'scroll up')
+config.bind('n', 'scroll right')
+config.bind('s', 'search-next')
+config.bind('l', 'search-prev')
+
+config.bind('j', 'scroll-page 0 1')
+config.bind('k', 'scroll-page 0 -1')
+
+config.bind('R', 'restart')
+config.bind(',', 'back')
+config.bind('.', 'forward')
